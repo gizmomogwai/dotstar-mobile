148a5196ea93b2c2b29c01f6a0f193c7e27c2b1b
Christian Koestlin <christian.koestlin@esrlabs.com>
2018-03-14 00:37:19 +0100

    Add utility to convert from serviceinfo to uri

--- lib/widgets/misc.dart
+++ lib/widgets/misc.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:mdns/mdns.dart';
 
 class ProgressWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new LinearProgressIndicator(value: null);
   }
+}
+
+Uri infoToUri(ServiceInfo i) {
+  final newUri = Uri.parse("http:/${i.host}:${i.port}");
+  return newUri;
 }
